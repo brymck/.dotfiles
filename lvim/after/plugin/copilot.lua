b4eda79b70ccca27327a344c34049004b0667e6e
Bryan McKelvey <bryan.mckelvey@gmail.com>
2024-01-05 23:09:37 +0900

    Use NvChad as base

---
+++ lvim/after/plugin/copilot.lua
@@ -0,0 +1,37 @@
+local ok, copilot = pcall(require, "copilot")
+if not ok then
+    return
+end
+
+copilot.setup {
+    suggestion = {
+        keymap = {
+            accept = "<c-l>",
+            next = "<c-j>",
+            prev = "<c-k>",
+            dismiss = "<c-h>",
+        },
+    },
+}
+
+vim.api.nvim_set_keymap("n", "<c-s>", "<cmd>lua require('copilot.suggestion').toggle_auto_trigger()<CR>", {
+    noremap = true,
+    silent = true,
+})
+
+-- Below config is required to prevent copilot overriding Tab with a suggestion
+-- when you're just trying to indent!
+local has_words_before = function()
+    if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+end
+local on_tab = vim.schedule_wrap(function(fallback)
+    local cmp = require("cmp")
+    if cmp.visible() and has_words_before() then
+        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+    else
+        fallback()
+    end
+end)
+lvim.builtin.cmp.mapping["<Tab>"] = on_tab
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,13 @@
-require("brymck")
+require "core"
+require("core.utils").load_mappings()
+
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+-- bootstrap lazy.nvim!
+if not vim.loop.fs_stat(lazypath) then
+  require("core.bootstrap").lazy(lazypath)
+end
+
+dofile(vim.g.base46_cache .. "defaults")
+vim.opt.rtp:prepend(lazypath)
+require "plugins"
