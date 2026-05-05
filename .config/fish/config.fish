5104fc67201a48a6e7e5c72ee03a64f64d698c8c
Bryan McKelvey <bryan.mckelvey@gmail.com>
2026-05-06 00:08:48 +0900

    Use fnm

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,13 @@ if test -x /opt/homebrew/bin/brew
 end
 
 #-------------------------------------------------------------------------------
+# Node
+#-------------------------------------------------------------------------------
+if test -x /opt/homebrew/bin/fnm
+    eval (/opt/homebrew/bin/fnm env)
+end
+
+#-------------------------------------------------------------------------------
 # Aliases
 #-------------------------------------------------------------------------------
 if status is-interactive
@@ -36,16 +43,3 @@ if status is-interactive
     abbr -a gsta 'git stash push'
     abbr -a gstp 'git stash pop'
 end
-
-#-------------------------------------------------------------------------------
-# ASDF
-#-------------------------------------------------------------------------------
-if test -z $ASDF_DATA_DIR
-    set _asdf_shims "$HOME/.asdf/shims"
-else
-    set _asdf_shims "$ASDF_DATA_DIR/shims"
-end
-if not contains $_asdf_shims $PATH
-    set -gx --prepend PATH $_asdf_shims
-end
-set --erase _asdf_shims
